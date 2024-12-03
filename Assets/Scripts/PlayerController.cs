0dbed88add466816d439d19aaa5ff1ed509411a8
tlpetrick <lpetrick@stud.hs-heilbronn.de>
2024-12-03 15:52:40 +0100

    add jump variety depending on hold

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,14 +12,17 @@ public class PlayerController : MonoBehaviour
     public LayerMask groundLayer;
 
     [Header("Movement")]
-    private bool isGrounded = false;
     private Vector2 direction;
+    public float movementSpeed;
+    public float maxSpeed = 6f;
+    public float accelerationSpeed = 25f;
+
+    [Header("Jumping")]
+    private bool isGrounded = false;
     private bool canDoubleJump = true;
-    public float movementSpeed = 5f;
     public float jumpForce = 500f;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
         rigidBody = GetComponent<Rigidbody2D>();
         playerCollider = GetComponent<Collider2D>();
@@ -33,6 +36,14 @@ public class PlayerController : MonoBehaviour
 
     private void Move()
     {
+        if (direction != Vector2.zero && movementSpeed < maxSpeed)
+        {
+            movementSpeed += Time.deltaTime * accelerationSpeed;
+        }
+        if (direction == Vector2.zero)
+        {
+            movementSpeed = 0f;
+        }
         rigidBody.velocity = (new Vector2(direction.x * movementSpeed, rigidBody.velocity.y));
     }
 
@@ -42,15 +53,15 @@ public class PlayerController : MonoBehaviour
         Vector2 boxCastSize = new Vector2(playerCollider.bounds.size.x, 0.02f);
 
         RaycastHit2D groundHit = Physics2D.BoxCast(boxCastOrigin, boxCastSize, 0f, Vector2.down, 0.02f, groundLayer);
-        
+
         if (groundHit.collider != null)
         {
-            isGrounded =  true;
+            isGrounded = true;
             canDoubleJump = true;
         }
-        else 
+        else
         {
-            isGrounded =  false; 
+            isGrounded = false;
         }
     }
 
@@ -63,14 +74,18 @@ public class PlayerController : MonoBehaviour
     {
         if (ctx.performed && isGrounded)
         {
-            rigidBody.velocity = new Vector2(direction.x ,0);
+            rigidBody.velocity = new Vector2(direction.x, 0);
             rigidBody.AddForce(new Vector2(0, jumpForce));
-        } 
-        else if (ctx.performed && !isGrounded && canDoubleJump) 
+        }
+        else if (ctx.performed && !isGrounded && canDoubleJump)
         {
             rigidBody.velocity = new Vector2(direction.x, 0);
             rigidBody.AddForce(new Vector2(0, jumpForce));
             canDoubleJump = false;
+        }
+        if (ctx.canceled && rigidBody.velocity.y >= 0)
+        {
+            rigidBody.velocity = new Vector2(direction.x, 0);
         }
     }
 }
