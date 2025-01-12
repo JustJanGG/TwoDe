3dd3dfec3b4055d1f403bb22102b44fa37aa1e83
JustJanGG <jan.haufer@gmail.com>
2025-01-12 17:22:40 +0100

    Added Pathfinding and Ghost enemy

--- Assets/Scripts/PlayerScripts/PlayerAnimation.cs
+++ Assets/Scripts/PlayerScripts/PlayerAnimation.cs
@@ -9,6 +9,7 @@ public class PlayerAnimation : MonoBehaviour
     public Transform staff;
     private PlayerController playerController;
     private Animator animator;
+    
     void Start()
     {
         animator = GetComponent<Animator>();
@@ -19,10 +20,15 @@ public class PlayerAnimation : MonoBehaviour
         if (staff.rotation.eulerAngles.z > 90 && staff.rotation.eulerAngles.z < 270)
         {
             animator.SetFloat("Facing", -1);
+            staff.localScale = new Vector3(1, -1, 1);
+            staff.localPosition = new Vector3(0.1f, 0.015f, -0.01f);
+  
         }
         else
         {
             animator.SetFloat("Facing", 1);
+            staff.localScale = new Vector3(1, 1, 1);
+            staff.localPosition = new Vector3(-0.1f, 0.015f, -0.01f); 
         }
 
         animator.SetFloat("Speed", Mathf.Clamp(playerController.GetVelocityX(), -1, 1));
